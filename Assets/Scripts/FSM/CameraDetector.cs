4d7b531b25f2ce02b9baa5056101a3e96d673777
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-10-28 11:52:42 +0100

    debugging

--- Assets/Scripts/FSM/CameraDetector.cs
+++ Assets/Scripts/FSM/CameraDetector.cs
@@ -9,9 +9,14 @@ public class CameraDetector : MonoBehaviour
     private FieldOfView fov;
     private bool playerInRange = false;
 
+    private StateController[] stateControllers;
+
     void Awake()
     {
         fov = GetComponent<FieldOfView>();
+        stateControllers = FindObjectsOfType<StateController>();
+
+        
     }
 
     // Update is called once per frame
@@ -20,8 +25,8 @@ public class CameraDetector : MonoBehaviour
         if (fov.visibleTargets.Count != 0 && !playerInRange)
         {
             playerInRange = true;
-            StateController[] stateControllers = FindObjectsOfType<StateController>();
-            foreach (StateController sC in stateControllers)
+            
+            /*foreach (StateController sC in stateControllers)
             {
                 if (!sC.wanderer && sC.currentState != sC.chaseState)
                 {
@@ -31,8 +36,18 @@ public class CameraDetector : MonoBehaviour
                     sC.aStarUnit.StartCoroutine(sC.aStarUnit.SearchPath(fov.visibleTargets[0]));
                     sC.TransitionToState(triangleAlertState);
                 }
+            }*/
+            if (stateControllers[2].currentState != stateControllers[2].chaseState){
+                    stateControllers[2].exclamation.SetActive(false);
+                    stateControllers[2].interrogation.SetActive(true);
+                    stateControllers[2].warnedPoint = fov.visibleTargets[0].position;
+                    stateControllers[2].aStarUnit.StartCoroutine(stateControllers[2].aStarUnit.SearchPath(fov.visibleTargets[0]));
+                    stateControllers[2].TransitionToState(triangleAlertState);
+                    Debug.Log("Cuadrado a la cámara");
+
             }
-        } else if (playerInRange && fov.visibleTargets.Count == 0){
+        } 
+        else if (playerInRange && fov.visibleTargets.Count == 0){
             playerInRange = false;
         }
     }
--- Assets/Scripts/FSM/LookDecision.cs
+++ Assets/Scripts/FSM/LookDecision.cs
@@ -26,21 +26,27 @@ public class LookDecision : Decision
             }
             else
             {
-                int i = 0;
+                
+                /*int i = 0;
                 StateController[] stateControllers = controller.allStateControllers;
                 while (stateControllers[i].wanderer)
                 {
                     i = Random.Range(0, stateControllers.Length);
                 }
 
-                StateController sC = stateControllers[i];
+                Debug.Log(i);*/
+                
+                StateController[] stateControllers = controller.allStateControllers;
+                StateController sC = stateControllers[0];
                 if (sC.currentState != sC.alertState && sC.currentState != sC.chaseState && controller.currentState != controller.scanState)
                 {
                     sC.exclamation.SetActive(false);
                     sC.interrogation.SetActive(true);
                     sC.warnedPoint = controller.fieldOfView.visibleTargets[0].position;
-                    sC.TransitionToState(sC.alertState);
                     sC.aStarUnit.StartCoroutine(sC.aStarUnit.SearchPath(controller.fieldOfView.visibleTargets[0]));
+                    sC.TransitionToState(sC.alertState);
+                    
+                    Debug.Log("Pentágono buscando al player");
                 }
 
                 sC = controller;
@@ -53,10 +59,14 @@ public class LookDecision : Decision
                     sC.warnedPoint = sC.piecesWardPoint.position;
                     sC.aStarUnit.StartCoroutine(sC.aStarUnit.SearchPath(sC.piecesWardPoint));
                     sC.TransitionToState(sC.protectPiecesState);
-                    for (int j = 0; j < 3; j++)
+                    
+                    Debug.Log("Triángulo protegiendo piezas");
+                    
+                    //no dejar que se recojan piezas si se está en busca y captura
+                    /*for (int j = 0; j < 3; j++)
                     {
                         if (sC.piecesCollider[j] != null) sC.piecesCollider[j].enabled = false;
-                    }
+                    }*/
                 }
 
                 /*    foreach (StateController sC in stateControllers)
